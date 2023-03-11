bf712af67fd1ca9c6dae22de482fc1453e7975fd
MultisampledNight <contact@multisamplednight.com>
2023-03-11 20:22:38 +0100

    Add evcxr to rust shell

--- nix/shells/rust/shell.nix
+++ nix/shells/rust/shell.nix
@@ -4,7 +4,7 @@ pkgs.mkShell rec {
   buildInputs = with pkgs; [
     clang llvmPackages_latest.lld llvmPackages.libclang
     openssl pkg-config webkitgtk.dev
-    rustup wasm-pack rust-analyzer
+    rustup wasm-pack rust-analyzer evcxr
 
     python3 valgrind
   ];
