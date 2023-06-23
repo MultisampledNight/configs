8eca2306903e8d0217d0bb006298bb071a0da051
MultisampledNight <contact@multisamplednight.com>
2023-06-23 19:28:21 +0200

    slight -> hintslight, back to 23.05 after unstable

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -298,7 +298,7 @@ in
     enableDefaultFonts = true;
 
     fontconfig = {
-      hinting.style = "slight";
+      hinting.style = "hintslight";
 
       # ...while this one sets the actually in-place default fonts
       defaultFonts = {
