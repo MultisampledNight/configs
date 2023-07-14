11774e7c51fe53a8c9c6a705ef629c1bb1575106
MultisampledNight <contact@multisamplednight.com>
2023-07-15 00:20:14 +0200

    Load Neovim plugins through Nix instead of vim-plug

---
+++ nixos/neovim/custom-plugins.nix
@@ -0,0 +1,19 @@
+# the nixpkgs manual suggests to have this to be generated automatically instead
+# but I don't want to have all that out-of-bounds state management for the few plugins I maintain anyway
+{ pkgs, lib, ... }:
+
+let
+  build = pkgs.vimUtils.buildVimPluginFrom2Nix;
+in final: prev: {
+  multisn8-colorschemes = build {
+    name = "multisn8-colorschemes";
+    version = "2023-07-14";
+    src = pkgs.fetchFromGitHub {
+      owner = "MultisampledNight";
+      repo = "colorschemes";
+      rev = "main";
+      sha256 = "sha256-SYpWCPR2ORrfraMuvTRoQ4zE9FSFHTpbIh9G+vElzK8=";
+    };
+    meta.homepage = "https://github.com/MultisampledNight/colorschemes";
+  };
+}
