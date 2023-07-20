683255e39d5e0862426bd55f7ecf43a7e9bba091
MultisampledNight <contact@multisamplednight.com>
2023-07-20 21:24:24 +0200

    Add common vulkan packages to rust shell

--- nix/shells/rust/default.nix
+++ nix/shells/rust/default.nix
@@ -2,15 +2,21 @@
 
 pkgs.mkShell rec {
   buildInputs = with pkgs; [
-    cargo rustc rust-analyzer rustup
+    rust-analyzer rustup
 
     clang llvmPackages_latest.lld llvmPackages.libclang
     openssl pkg-config
     wasm-pack evcxr tokei
-    libGL directx-shader-compiler
     libxkbcommon
     wayland xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi
     fontconfig freetype
+
+    shaderc directx-shader-compiler
+    libGL
+    vulkan-headers vulkan-loader
+    vulkan-tools vulkan-tools-lunarg
+    vulkan-validation-layers vulkan-extension-layer
+    monado openxr-loader openxr-loader.dev
 
     mdbook mdbook-linkcheck mdbook-pagetoc
 
