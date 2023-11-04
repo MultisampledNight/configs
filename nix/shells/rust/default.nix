3ec336506752613e74815126d99dd0ea4392706d
MultisampledNight <contact@multisamplednight.com>
2023-11-04 20:43:23 +0100

    feat(shells/rust): switch to nightly

--- nix/shells/rust/default.nix
+++ nix/shells/rust/default.nix
@@ -3,7 +3,7 @@
 pkgs.mkShell rec {
   buildInputs = with pkgs; [
     rustup
-    cargo-audit cargo-nextest cargo-flamegraph
+    cargo-audit cargo-expand cargo-flamegraph cargo-nextest
 
     gdb lldb
     clang llvmPackages.libclang
@@ -44,7 +44,7 @@ pkgs.mkShell rec {
     export PATH="$PATH:''${RUSTUP_HOME:-~/.rustup/toolchains/$RUSTC_VERSION-x86_64-unknown-linux/bin}"
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
 
-    rustup default stable
+    rustup default $RUSTC_VERSION
     rustup component add rustfmt clippy rust-src rust-analyzer
   '';
 }
