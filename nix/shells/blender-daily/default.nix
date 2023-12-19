1fc7de34471c4d969c4afbb05f79db1ec0bb8522
MultisampledNight <contact@multisamplednight.com>
2023-12-19 21:58:46 +0100

    fix(shells/blender-daily): add more deps

--- nix/shells/blender-daily/default.nix
+++ nix/shells/blender-daily/default.nix
@@ -6,8 +6,10 @@ mkShell {
     libz
     libGL
     wayland
+    fontconfig
   ] ++ (with xorg; [
     libXext libICE libX11 libSM libXi libxkbcommon libXfixes libXrender libXxf86vm libX11
+    libXcursor libXinerama libXrandr
   ]));
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
 }
