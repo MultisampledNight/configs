45cfbd10aad66f27cebd530607eff3e06e1e2aed
MultisampledNight <contact@multisamplednight.com>
2024-09-20 16:10:21 +0200

    feat(nixos): rice sudo and make it more comfy

--- nixos/generalized.nix
+++ nixos/generalized.nix
@@ -5,6 +5,23 @@
 with lib;
 let
   cfg = config.generalized;
+
+  indent = strings.replicate 7 " ";
+
+  # see https://en.wikipedia.org/wiki/ANSI_escape_code
+  # why they are exactly here? good question, had no other place to put them
+  # if anyone has an idea on how to escape this in nix, let me know
+  esc = "";
+  csi = params: op: esc + "[" + params + op;
+
+  sgr = n: csi (toString n) "m";
+  cha = n: csi (toString n) "G";
+  reset = "${esc}(B" + (csi "" "m");
+
+  fg = idx: sgr (30 + idx);
+
+  query = "${fg 4}>${reset}";
+  error = "${fg 1}!${reset}";
 in {
   options.generalized = {
     hostName = mkOption {
@@ -434,6 +451,25 @@ in {
       enable = true;
       platformTheme = "qt5ct";
     };
+
+    security.sudo.extraConfig = concatStringsSep "\n" (
+      mapAttrsToList (name: value: "Defaults " + (
+        if (isBool value) then
+          (optionalString (!value) "!") + name
+        else if (isString value) then
+          "${name}=\"${value}\""
+        else
+          "${name}=${toString value}"
+      )) {
+        # see sudoers(5)
+        passwd_timeout = 0;
+        timestamp_type = "global";
+
+        passprompt = "${indent}${query} auth for %u${cha 8}";
+        badpass_message = "${indent}${error} wrong password";
+        authfail_message = " ${error} %d time(s) incorrect";
+      }
+    );
 
     virtualisation = {
       libvirtd.enable = true;
