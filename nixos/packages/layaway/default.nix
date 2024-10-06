e30b0a2ef525174095c9eb3bae2f07924f4dda07
MultisampledNight <contact@multisamplednight.com>
2024-10-06 21:33:57 +0200

    update(layaway): 0.1.6 → 0.2.0

--- nixos/packages/layaway/default.nix
+++ nixos/packages/layaway/default.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "layaway";
-  version = "0.1.6";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "MultisampledNight";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-HJcw/OeUVNVMz/wsxJPqUQKpZBJ16JqSv2d4zqsvUds=";
+    hash = "sha256-SzAuVFEy56svasO3+1p6ysBRrIQd0UZX++/P4ZuwWm0=";
   };
 
-  cargoHash = "sha256-m8aAhIQLVWOPuUWjzbTauZ6sN/xbtgj48Hq6XBpjHcc=";
+  cargoHash = "sha256-liWP6AI72xG1O+MbCZ0cjJ2llHj/iv3hR/U3BLv5fKA=";
 
   meta = with lib; {
     description = "Layout creation for Sway via a relative and human-readable DSL.";
